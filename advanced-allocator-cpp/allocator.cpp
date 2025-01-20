af88e3c84357cfbcd35f7cd917f982d2d51676e6
DaryanZabear <71202670+Daiyan-Zubaier@users.noreply.github.com>
2025-01-20 16:30:16 -0500

    Minor changes to allocator.cpp

--- advanced-allocator-cpp/allocator.cpp
+++ advanced-allocator-cpp/allocator.cpp
@@ -1,5 +1,7 @@
 #include <cstdint>
 
+//intptr_t is our system architecture's word size
+
 struct MemBlock{
     //Info about memory block
     std::size_t size; //Check size of block
@@ -11,6 +13,15 @@ struct MemBlock{
     //Data
     //Beginning of data
     intptr_t data[1];
+};
 
-    
+//Does memory allignment
+inline std::size_t allign(std::size_t org_size){
+    return org_size + sizeof(intptr_t) - org_size % sizeof(intptr_t);
 }
+
+//Allocator
+intptr_t *alloc(std::size_t size){
+    allign(size);
+}
+
